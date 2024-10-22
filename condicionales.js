508f01d4184706875a5974f44a60acef7da5785d
Santiago Fontana <tatifontana14@gmail.com>
2024-10-22 19:51:25 -0300

    Agregando condicionales, actualizando el title del archivo html y agregando la parte uno de la historia de cofla

---
+++ condicionales.js
@@ -0,0 +1,14 @@
+
+let nombre = "pepe"
+
+if (nombre == "Santiago") {
+    alert(`tu nombre es ${nombre}`)
+}
+
+else if (nombre == "Fontana") {
+    alert(`tu nombre es ${nombre}`)
+}
+
+else {
+    alert("tu nombre es otro")
+}
