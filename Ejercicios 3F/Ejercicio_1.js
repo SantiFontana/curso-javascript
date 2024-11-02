f3394058a4fc09129b31f90647a27c7483b61771
Santiago Fontana <tatifontana14@gmail.com>
2024-11-02 18:05:15 -0300

    Haciendo el ejercicio 1 de 3F

---
+++ Ejercicios 3F/Ejercicio_1.js
@@ -0,0 +1,11 @@
+let numero = parseInt(prompt("Ingresa un número entero: "));
+
+if (numero > 0) {
+    alert("Positivo");
+}
+else if (numero < 0) {
+    alert("Negativo");
+}
+else {
+    alert("Neutral");
+}
